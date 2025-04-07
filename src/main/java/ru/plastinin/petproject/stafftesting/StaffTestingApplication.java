55159e70a025ee5c4d773fe83234822b7394c5a4
Plastinin-Igor <igor-plastinin@yandex.ru>
2025-04-07 15:12:02 +0300

    add files schema.sql

--- src/main/java/ru/plastinin/petproject/stafftesting/StaffTestingApplication.java
+++ src/main/java/ru/plastinin/petproject/stafftesting/StaffTestingApplication.java
@@ -1,7 +1,9 @@
 package ru.plastinin.petproject.stafftesting;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class StaffTestingApplication {
     public static void main(String[] args) {
         SpringApplication.run(StaffTestingApplication.class, args);
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+MERGE INTO users (user_id,email,login,NAME,birthday) VALUES
+	 (1, 'igor-plastinin@yandex.ru','Plastinin','Plastinin Igor','1984-08-12');
