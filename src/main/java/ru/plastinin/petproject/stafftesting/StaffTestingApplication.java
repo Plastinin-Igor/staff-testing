a680706fbdc458daa70ed1316db8a2feff818de0
Plastinin-Igor <igor-plastinin@yandex.ru>
2025-04-07 13:44:45 +0300

    init commit

---
+++ src/main/java/ru/plastinin/petproject/stafftesting/StaffTestingApplication.java
@@ -0,0 +1,9 @@
+package ru.plastinin.petproject.stafftesting;
+
+import org.springframework.boot.SpringApplication;
+
+public class StaffTestingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StaffTestingApplication.class, args);
+    }
+}
---
+++ src/main/java/ru/plastinin/petproject/stafftesting/model/subject.java
@@ -0,0 +1,4 @@
+package ru.plastinin.petproject.stafftesting.model;
+
+public class subject {
+}
